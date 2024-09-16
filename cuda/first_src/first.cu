6f650a1d59613663aac5b1e318d15250b693d928
Iliass Lasri <iliass.lasri@outlook.com>
2024-09-16 20:12:20 +0200

    first cude code

---
+++ cuda/first_src/first.cu
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+__global__ void kernel() {
+    printf("Hello, CUDA!\n");
+}
+
+void checkCudaError(cudaError_t result, const char* msg) {
+    if (result != cudaSuccess) {
+        std::cerr << "CUDA Error: " << msg << " (" << cudaGetErrorString(result) << ")" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main() {
+    kernel<<<1, 1>>>();
+    checkCudaError(cudaGetLastError(), "Kernel launch");
+    checkCudaError(cudaDeviceSynchronize(), "Device synchronization");
+    return 0;
+}
